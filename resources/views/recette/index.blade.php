aa0ae528d7144ae2112554bae4e0c6511630b3dd
Itega <julien.zolli1@gmail.com>
2017-03-31 16:52:49 +0200

    view: Edit recette

--- resources/views/recette/index.blade.php
+++ resources/views/recette/index.blade.php
@@ -2,14 +2,36 @@
 
 @section('content')
     <div class="container">
-        <ul>
-            @foreach ($recettes as $recette)
-                <li>
-                    <a href="{{ route('recette.show', $recette->ID_PRD) }}">
-                        {{ $recette->PRD_NOM }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2>Liste des produits</h2>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($recettes as $recette)
+                    <tr>
+                        <th>
+                            {{ $loop->iteration }}
+                        </th>
+                        <th>
+                            <a href="{{ route('recette.show', $recette->ID_PRD) }}">
+                                {{ $recette->PRD_NOM }}
+                            </a>
+                        </th>
+                        <th>
+                            <p>{{ $recette->PRD_PRIX }}</p>
+                        </th>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
--- resources/views/recette/show.blade.php
+++ resources/views/recette/show.blade.php
@@ -5,7 +5,10 @@
     <div class="container">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h2>{{ $recette[0]->PRD_NOM }}</h2>
+                <h2 style="display:inline-block;">{{ $recette[0]->PRD_NOM }}</h2>
+                @if ($recette[0]->PRD_IS_POTION)
+                    <h3 class="pull-right">Diluant : {{ $recette[0]->PRD_DILUANT }}</h3>
+                @endif
             </div>
             <table class="table table-striped table-hover">
                 <thead>
@@ -15,26 +18,22 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                @if ($recette[0]->PRD_IS_POTION)
-                    <td>{{ $recette[0]->PRD_DILUANT }}</td>
-                @endif
-
                 @foreach ($ingredients as $key => $ingredient)
-                    <td>
-                        {{--<a href="{{ route('ingredient.show', $ingredient->ID_NGR) }}">--}}
-                            {{ $ingredient->ID_NGR }}
-                        {{--</a>--}}
-                    </td>
-                    <td>
-                        {{--<a href="{{ route('ingredient.show', $ingredient->ID_NGR) }}">--}}
-                            {{ $ingredient->NGR_NOM }}
-                        {{--</a>--}}
-                    </td>
-                @endforeach
+                    <tr>
+                        <td>
+                            <p>{{ $loop->iteration }}</p>
+                        </td>
+                        <td>
+                            <a href="{{ route('ingredient.index').'#'.$ingredient->ID_NGR }}">
+                                {{ $ingredient->NGR_NOM }}
+                            </a>
+                        </td>
                     </tr>
+                @endforeach
+
                 </tbody>
             </table>
         </div>
+        <a href="{{ route('recette.index', $ingredient->ID_NGR) }}" class="btn btn-primary center-block">Retour à la liste</a>
     </div>
 @endsection
