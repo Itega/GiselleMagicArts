069404e9f8939bf3d6ab6f9b9befdd9bb7126bf4
Dorian Lecoeur <contact@lecoeurd.fr>
2017-04-01 18:13:56 +0200

    view: Add inventeur

---
+++ resources/views/inventeur/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2>Liste des inventeurs</h2>
+            </div>
+
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nom</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($inventeurs as $k => $inventeur)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>
+                                <a href="{{ route('inventeur.show', $inventeur->ID_NVN) }}">
+                                    {{ $inventeur->NVN_NOM }}
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
