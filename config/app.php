56614a334322a73299b60048da5edb0a69fadf29
Dorian Lecoeur <contact@lecoeurd.fr>
2017-04-03 10:27:09 +0200

    config: Add DebugBar

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -230,6 +231,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
