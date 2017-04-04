4a7eea0de4fcb1b0cb23bcaef5efaab5dae716b2
Dorian Lecoeur <contact@lecoeurd.fr>
2017-04-04 14:55:58 +0200

    controller: Edit produit

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -29,7 +29,22 @@ class ProduitController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create() {
+        $recettes = DB::select(DB::raw('
+            SELECT ID_RCT, RCT_NOM FROM recette
+        '));
+        foreach ($recettes as $k => $recette)
+            $tmp[$recette->ID_RCT] = $recette->RCT_NOM;
+        $recettes = $tmp;
 
+        $diluants = DB::select(DB::raw('
+            SELECT ID_DLN, DLN_NOM FROM diluant
+        '));
+        $tmp[null] = 'Pas de diluant';
+        foreach ($diluants as $k => $diluant)
+            $tmp[$diluant->ID_DLN] = $diluant->DLN_NOM;
+        $diluants = $tmp;
+
+        return view('produit.create', compact('recettes', 'diluants'));
     }
 
     /**
@@ -49,6 +64,24 @@ class ProduitController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id) {
+        $produit = DB::select(DB::raw('
+            SELECT * FROM produit
+            WHERE ID_PRD = '. $id
+        ));
+        $produit = $produit[0];
+        $produit->diluant = DB::select(DB::raw('
+            SELECT DLN_NOM FROM diluant
+            WHERE ID_DLN = '. $produit->ID_DLN
+        ));
+        $produit->diluant = $produit->diluant[0];
+        $produit->recette = DB::select(DB::raw('
+            SELECT RCT_NOM, RCT_TEMPERATURE FROM recette
+            WHERE ID_RCT = '. $produit->ID_RCT .'
+            AND RCT_VALIDEE = 1
+        '));
+        $produit->recette = $produit->recette[0];
+
+        return view('produit.show', compact('produit'));
     }
 
     /**
