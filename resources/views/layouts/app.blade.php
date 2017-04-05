5ec6b96c43536b9c69149f69a5a82307b384dc82
Dorian Lecoeur <contact@lecoeurd.fr>
2017-04-05 11:10:14 +0200

    view: Edit layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,35 +45,16 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
+                        <li><a href="{{ route('ingredient.index') }}">Ingrédients</a></li>
+                        <li><a href="{{ route('recette.index') }}">Recettes</a></li>
+                        <li><a href="{{ route('inventeur.index') }}">Inventeurs</a></li>
+                        <li><a href="{{ route('fournisseur.index') }}">Fournisseur</a></li>
+                        <li><a href="{{ route('produit.index') }}">Produit</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Se connecter</a></li>
-                            <li><a href="{{ route('register') }}">S'enregistrer</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
                     </ul>
                 </div>
             </div>
